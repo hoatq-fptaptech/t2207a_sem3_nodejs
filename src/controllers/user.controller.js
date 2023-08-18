2e0649430f2f1913989eac16f75d38779905c451
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-08-18 09:34:56 +0700

    crypt

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,21 @@
 const User = require("./../models/user.model");
+const bcrypt = require("bcryptjs");
 exports.register = (req,res)=>{
     res.render("register");
 }
-exports.postRegister = (req,res)=>{
-    const data = req.body;
-    const u = new User(data);
-    u.save().then(()=>{
+exports.postRegister = async (req,res)=>{
+    try{
+        const data = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashed = await bcrypt.hash(data.password,salt);
+        //
+       // bcrypt.compare
+        data.password = hashed;
+        const u = new User(data);
+        await u.save();
         res.send("DONE");
-    }).catch(err=>{
+    }catch(err){
         res.send(err);
-    })
+    }
     
 }
