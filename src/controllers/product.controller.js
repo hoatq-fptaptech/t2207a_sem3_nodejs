26d7b147ae66bac790cd1aa7231661efcd0cdf56
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-08-21 16:52:55 +0700

    CRUD

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,6 +10,8 @@ exports.list = async (req,res)=>{
 }
 exports.formCreate = (req,res)=>{
     const data = req.body;
+    // console.log(req._parsedOriginalUrl.path)
+    data.url = req._parsedOriginalUrl.path;
     res.render("product/form",{product:data});
 }
 exports.store = async (req,res)=>{
@@ -30,5 +32,49 @@ exports.store = async (req,res)=>{
         res.redirect("/product");
     } catch (error) {
         res.render("product/form",{product:data,error:error});
+    }
+}
+
+exports.formEdit = async (req,res)=>{
+    const _id = req.params.id;
+    try {
+        const product = await Product.findById(_id);
+        product.url = req._parsedOriginalUrl.path;
+        res.render("product/form",{product:product});
+    } catch (error) {
+        res.redirect("/product");
+    }
+    
+}
+
+exports.update = async (req,res)=>{
+    const _id = req.params.id;
+    const data= req.body;
+    const product = await Product.findById(_id);
+    try {
+        const file = req.file;
+        if(file){
+            const img = fs.readFileSync(file.path);
+            data.thumbnail = {
+                contentType: file.mimetype,
+                data:img.toString("base64")
+            }
+        }else{
+            data.thumbnail = product.thumbnail;
+        }
+        await Product.findByIdAndUpdate(_id,data);
+        res.redirect("/product");
+    } catch (error) {
+        res.render("product/form",{product:product});
+    }
+}
+
+exports.delete =  async (req,res)=>{
+    const _id = req.params.id;
+    try {
+        await Product.findByIdAndDelete(_id);
+        res.redirect("/product");
+    } catch (error) {
+        res.redirect("/product");
     }
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -6,10 +6,12 @@ const controller = require("./../controllers/product.controller");
 const multer = require("multer");
 const storage = multer.diskStorage({
     destination: (req,file,callback)=>{
-        callback(null,"public/uploads");
+        if(file)
+            callback(null,"public/uploads");
     },
     filename: (req,file,callback)=>{
-        callback(null,Date.now()+"-"+file.originalname);
+        if(file)
+            callback(null,Date.now()+"-"+file.originalname);
     }
 });
 const upload = multer({storage:storage});
@@ -17,7 +19,8 @@ const upload = multer({storage:storage});
 router.get("/",controller.list);
 router.get("/create",controller.formCreate);
 router.post("/create",upload.single("thumbnail"),controller.store);
-// router.get("/edit/:id",controller.formCreate);
-// router.post("/create",controller.store);
+router.get("/edit/:id",controller.formEdit);
+router.post("/edit/:id",upload.single("thumbnail"),controller.update);
+router.get("/delete/:id",controller.delete);
 
 module.exports = router;
